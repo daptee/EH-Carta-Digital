80c145c3ff875e0b32309dce9ee7f6dcba010613
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2025-12-26 17:54:56 -0300

    fix

--- components/UniqueProduct.vue
+++ components/UniqueProduct.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="flex gap-[30px] items-center grow">
 
-        <ImagePopup v-if="principalImage" :image="`${config.public.OUR_EH_CAFETERIA_BASE_URL}${principalImage}`" class="w-[75px] h-[75px]" />
+        <ImagePopup v-if="principalImage" :image="`${config.public.OUR_EH_CAFETERIA_BASE_URL}${principalImage}`"
+            class="w-[75px] h-[75px]" />
         <div v-else class="w-[75px] h-[75px] border-[4px] rounded-full border-[#656874]">
             <IconsPlaceholder class="w-full h-full border-[4px] rounded-full border-[#848690]" />
         </div>
-        
-        <div class="flex justify-between grow gap-[9px]">
-            <p class="text-[26px] font-medium text-white">{{ getProductName(props.product, appStore.language) }}</p>
-            <div class="border-b-[0.5px] border-white grow mb-[15px]" />
-            <p class="text-[26px] font-medium text-white">${{
-                formatPrice(Number(getPrice(props.product.VARIANTES1[0].VARIANTES2[0], appStore.language))) }}</p>
-        </div>
+
+        <section class="flex flex-col grow">
+            <div class="flex justify-between grow gap-[9px]">
+                <p class="text-[26px] font-medium text-white">{{ getProductName(props.product, appStore.language) }}</p>
+                <div class="border-b-[0.5px] border-white grow mb-[15px]" />
+                <p class="text-[26px] font-medium text-white">${{
+                    formatPrice(getPrice(props.product.VARIANTES1[0].VARIANTES2[0], appStore.language), appStore.language) }}</p>
+            </div>
+
+            <div v-if="hasDescription(props.product.VARIANTES1[0].VARIANTES2[0])" class="max-w-[650px]">
+                <p class="text-[22px] font-light text-white">
+                    {{ getDescription(props.product.VARIANTES1[0].VARIANTES2[0], appStore.language) }}
+                </p>
+            </div>
+        </section>
     </div>
 </template>
 
 <script setup lang="ts">
 import { useAppStore } from '~/store/appStore';
+import type { Variant2 } from '~/types/Products';
 import type { UniqueProductProps } from '~/types/UniqueProduct';
 import { getProductName, getPrice } from '~/utils/getTranslates';
 
@@ -27,5 +37,9 @@ const appStore = useAppStore()
 const principalImage = computed(() => {
     return props.product.images.find(img => img.principal_image === 1)?.url
 })
+
+const hasDescription = (variant: Variant2) => {
+    return variant.DESCRIPCION !== '' || variant.DESCRIPCION_ENG !== '' || variant.DESCRIPCION_POR !== ''
+}
 
 </script>
--- utils/formatPrices.ts
+++ utils/formatPrices.ts
@@ -1,8 +1,19 @@
-export const formatPrice = (value: number | string) => {
+export const formatPrice = (value: number | string, language: string) => {
   if (!value) return '0';
-  const number = typeof value === 'string' ? parseFloat(value) : value;
+
+  const number =
+    typeof value === 'string'
+      ? Number(
+          value
+            .replace(/\./g, '') // quita separadores de miles
+            .replace(',', '.')  // convierte coma decimal
+        )
+      : value;
+
+  if (isNaN(number)) return '0';
+
   return number.toLocaleString('es-AR', {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
+    minimumFractionDigits: language === 'es' ? 0 : 2,
+    maximumFractionDigits: language === 'es' ? 0 : 2
   });
 };
